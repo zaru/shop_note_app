f7720ea41cdc0aedd14c775f505ed47d66b0eb32
有吉　貴紀 <takanori30907@MacBook-Pro.local>
2020-06-28 19:24:15 +0900

    グループ機能実装1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,19 +1,42 @@
 class GroupsController < ApplicationController
 
-  # def new
-  # end
-  #
-  # def create
-  #   @group = Groups.new(params)
-  # end
-  #
-  # def edit
-  # end
-  #
-  # def update
-  # end
-  #
-  # def destroy
-  # end
+  def index
+  end
+
+  def new
+  end
+
+  def create
+
+    @group = Group.new(group_params)
+
+    @group.admin_user_id = current_user.id
+    @group.save
+
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+
+    @group = Group.find(params[:id])
+     if @group.update_attributes(group_params)
+       redirect_to root_path
+     else
+       render 'groups/edit'
+     end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def group_params
+      params.permit(:name,:profile)
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     post "count", :to => "notes#count"
     post "comment", :to => "comments#create"
     delete "comments/:id",:to => "comments#destroy"
+    post "groups/:id/",:to => "groups#update"
 
 
     resources :groups
